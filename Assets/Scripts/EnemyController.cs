23742cf7975ae9b9b62957c901fbf5cecee0b3c2
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-12-01 20:43:43 +0900

    몬스터 리스폰 및 애니메이션
    몬스터 애니메이션 추가와 몬스터 리스폰 구현

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent nv;
+    private Animator anim;
+    private Transform monsterTr;
+    private Transform targetTr;
+    
+
+    void Start()
+    {
+        nv = this.GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        targetTr = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nv.destination != targetTr.position)
+        {
+            nv.SetDestination(targetTr.position);
+            anim.SetTrigger("run");
+
+        }
+
+        
+        //else
+        //{
+        //    nv.SetDestination(this.transform.position);
+        //    anim.SetBool("isFinish", true);
+        //}
+
+    }
+
+
+}
---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    private Transform[] points;
+    public GameObject monsterPrefab;
+    public float createTime;
+    public int maxMonster = 5;
+    public bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = GameObject.Find("ReSpawn").GetComponentsInChildren<Transform>();
+
+        if (points.Length > 0)
+        {
+            StartCoroutine(this.CreateMonster());
+        }
+    }
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator CreateMonster()
+    {
+        while (!isGameOver)
+        {
+            int monsterCnt = (int)GameObject.FindGameObjectsWithTag("Monster").Length;
+
+            if(monsterCnt < maxMonster)
+            {
+                yield return new WaitForSeconds(createTime);
+
+                int idx = Random.Range(1, points.Length);
+                Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+}
