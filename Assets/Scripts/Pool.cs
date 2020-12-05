b5b24932fd502d8934678d3c6815790690661fd3
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-12-05 16:44:58 +0900

    VR Controller Movement
    컨트롤러를 이용한 움직임 구현 및 blaster Resource 업로드

---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool
+{
+ public static List<string>
+}
+
+public class ProjectilePool : Pool
+{
+
+}
