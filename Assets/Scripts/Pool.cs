fb20d9a8f3cf485cc69cde5dd5df9b147956f8f4
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-12-05 18:43:18 +0900

    Blaster Weapon
    무기구현 및 오른쪽 컨트롤러로 조작 구현

--- Assets/Scripts/Pool.cs
+++ Assets/Scripts/Pool.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class Pool
 {
- public static List<string>
+    public  List<T> Create<T>(GameObject prefab, int count)
+        where T : MonoBehaviour
+    {
+        // New list
+        List<T> newPool = new List<T>();
+
+
+        // Create
+        for(int i=0; i<count; i++)
+        {
+            GameObject projectileObject = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            T newProjectile = projectileObject.GetComponent<T>();
+
+            newPool.Add(newProjectile);
+        }
+
+        return newPool;
+    }
 }
 
 public class ProjectilePool : Pool
 {
+    public List<Projectile> m_Projectiles = new List<Projectile>();
 
+    public ProjectilePool(GameObject prefab, int count)
+    {
+        m_Projectiles = Create<Projectile>(prefab, count);
+    }
+
+    public void SetAllProjectiles()
+    {
+        foreach(Projectile projectile in m_Projectiles)
+        {
+            projectile.SetInnactive();
+        }
+    }
 }
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -121,6 +121,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Reload
+        {
+            get
+            {
+                return SteamVR_Actions.default_Reload;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
